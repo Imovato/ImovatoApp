2e21a8585e1c1055865c4a7dd736e276f68bc095
Samuel <samuelmodestoes@gmail.com>
2022-05-27 21:39:58 -0300

    init LoginPage

--- lib/main.dart
+++ lib/main.dart
@@ -5,75 +5,60 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              backgroundColor: Colors.red,
-            ),
-            body: SafeArea(
-              child: Column(
-
-                children: [
-                  Container(
-                    color: Colors.green,
-                    child: const Image(
-                        width: 200,
-                        image: AssetImage('assets/casa-chique1.png'
-                        )),
-      
-                  ), 
-                  Text('ola mundo')
-                  
-                ],
-                // child: Center(
-                //     child: Image(
-                //   image: AssetImage('assets/casa-chique1.png'),
-                // )),
+      home: Scaffold(
+        appBar: AppBar(
+            backgroundColor: Color.fromARGB(
+          255,
+          56,
+          65,
+          82,
+        )),
+        body: SafeArea(
+          child: Column(
+            children: [
+              Container(
+                child: const Image(
+                  width: 400,
+                  image: AssetImage('assets/casa-chique1.png'),
+                ),
               ),
-
-            )
-
-          //     Image(
-          //   image: AssetImage('assets/casa-chique1.png'),
-          // ),
-
-          // ignore: prefer_const_literals_to_create_immutables
-          //     gradient: LinearGradient(colors: [
-          //   Color.fromAR9GB(255, 33, 41, 57),
-          //   Color.fromARGB(255, 56, 65, 82),
-          // ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
-          // child: Image(
-          //   image: AssetImage('assets/casa-chique1.png'),
-          //   width: 500,
-          //   height: 500,
-        ));
-
-    // return Stack(
-    //   children: <Widget>[
-    //     _buildBodyBack(),
-    //     CustomScrollView(
-    //       // ignore: prefer_const_literals_to_create_immutables
-    //       slivers: <Widget>[
-    //         SliverAppBar(
-    //           floating: true,
-    //           snap: true,
-    //           backgroundColor: Colors.red,
-    //           elevation: 0.0,
-    //           flexibleSpace: FlexibleSpaceBar(
-    //             title: Text('Novidades'),
-    //             centerTitle: true,
-    //           ),
-    //         ),
-    //
-    //         // Center(child: Image(image: AssetImage('assets/casa-chique1.png')))
-    //         // Image(image: AssetImage('assets/casa-chique1.png'))
-    //       ],
-    //     )
-    //   ],
-    // );
+              Container(
+                child: const Image(
+                  width: 400,
+                  image: AssetImage('assets/casa-chique1.png'),
+                ),
+              ),
+              Row(
+                children: [
+                  Column(
+                    children: const [
+                      Image(
+                        width: 150,
+                        image: AssetImage('assets/casa-chique1.png'),
+                      ),
+                      Image(
+                        width: 150,
+                        image: AssetImage('assets/casa-chique1.png'),
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: const [
+                      Image(
+                        width: 150,
+                        image: AssetImage('assets/casa-chique1.png'),
+                      ),
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
-
