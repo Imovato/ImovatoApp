3b580c64fd0f11b12b2ae23f5cbefc62da4ffc98
WillianGiovanni <willianclemente10@gmail.com>
2022-06-01 11:27:48 -0300

    feature/loginPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,16 @@
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
+
 import 'package:flutter/material.dart';
+import 'package:imovatoapp/login_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              backgroundColor: Colors.red,
-            ),
-            body: SafeArea(
-              child: Column(
-
-                children: [
-                  Container(
-                    color: Colors.green,
-                    child: const Image(
-                        width: 200,
-                        image: AssetImage('assets/casa-chique1.png'
-                        )),
-      
-                  ), 
-                  Text('ola mundo')
-                  
-                ],
-                // child: Center(
-                //     child: Image(
-                //   image: AssetImage('assets/casa-chique1.png'),
-                // )),
-              ),
-
-            )
-
-          //     Image(
-          //   image: AssetImage('assets/casa-chique1.png'),
-          // ),
-
-          // ignore: prefer_const_literals_to_create_immutables
-          //     gradient: LinearGradient(colors: [
-          //   Color.fromAR9GB(255, 33, 41, 57),
-          //   Color.fromARGB(255, 56, 65, 82),
-          // ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
-          // child: Image(
-          //   image: AssetImage('assets/casa-chique1.png'),
-          //   width: 500,
-          //   height: 500,
-        ));
-
-    // return Stack(
-    //   children: <Widget>[
-    //     _buildBodyBack(),
-    //     CustomScrollView(
-    //       // ignore: prefer_const_literals_to_create_immutables
-    //       slivers: <Widget>[
-    //         SliverAppBar(
-    //           floating: true,
-    //           snap: true,
-    //           backgroundColor: Colors.red,
-    //           elevation: 0.0,
-    //           flexibleSpace: FlexibleSpaceBar(
-    //             title: Text('Novidades'),
-    //             centerTitle: true,
-    //           ),
-    //         ),
-    //
-    //         // Center(child: Image(image: AssetImage('assets/casa-chique1.png')))
-    //         // Image(image: AssetImage('assets/casa-chique1.png'))
-    //       ],
-    //     )
-    //   ],
-    // );
+    return MaterialApp(home: LoginPage() // troque aq para ver as telas
+        );
   }
 }
-
