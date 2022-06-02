34fd10bec2edfa1976cc0da36fed16949cc7d0d6
WillianGiovanni <willianclemente10@gmail.com>
2022-06-01 21:55:34 -0300

    refactor: change button and create login_controller

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,9 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: LoginPage() // troque aq para ver as telas
-        );
+    return MaterialApp(
+      home: LoginPage(), // troque aq para ver as telas
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
