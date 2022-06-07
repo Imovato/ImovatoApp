2bcb1fd7b06ef6a4af51b7cf693ee6ed8b2e1b55
WillianGiovanni <willianclemente10@gmail.com>
2022-06-06 23:54:00 -0300

    feat: find property page and drawer

---
+++ lib/screens/find_property.dart
@@ -0,0 +1,319 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers
+
+import 'dart:ffi';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:imovatoapp/screens/login_page.dart';
+
+class FindPropertyPage extends StatefulWidget {
+  const FindPropertyPage({Key? key}) : super(key: key);
+
+  @override
+  State<FindPropertyPage> createState() => _FindPropertyPageState();
+}
+
+class _FindPropertyPageState extends State<FindPropertyPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.only(top: 70.0, left: 20.0),
+            children: [
+              Text(
+                'Olá, fulano',
+                textAlign: TextAlign.start,
+                style: GoogleFonts.robotoMono(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+              GestureDetector(
+                child: Text('Sair',
+                    style: GoogleFonts.robotoMono(
+                      color: Colors.blue,
+                      fontSize: 20,
+                    )),
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: ((context) => LoginPage())));
+                },
+              ),
+              SizedBox(
+                height: 100.0,
+              ),
+              GestureDetector(
+                child: Container(
+                  child: Row(
+                    children: [
+                      SvgPicture.asset('assets/homeIcon.svg'),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        'Início',
+                        style: GoogleFonts.robotoMono(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                onTap: () {},
+              ),
+              SizedBox(
+                height: 50.0,
+              ),
+              GestureDetector(
+                child: Container(
+                  child: Row(
+                    children: [
+                      SvgPicture.asset('assets/reservationIcon.svg'),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        'Reservas',
+                        style: GoogleFonts.robotoMono(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                onTap: () {},
+              ),
+              SizedBox(
+                height: 50.0,
+              ),
+              GestureDetector(
+                child: Container(
+                  child: Row(
+                    children: [
+                      SvgPicture.asset('assets/locationIcon.svg'),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        'Encontre um imóvel',
+                        style: GoogleFonts.robotoMono(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                onTap: () {},
+              ),
+            ],
+          ),
+          backgroundColor: Color.fromARGB(255, 56, 65, 82),
+        ),
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                "Imovato",
+                style: GoogleFonts.robotoMono(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+            ],
+          ),
+          backgroundColor: Color.fromARGB(255, 33, 41, 57),
+        ),
+        body: Form(
+          key: _formKey,
+          child: Container(
+            padding: EdgeInsets.all(30.0),
+            child: ListView(
+              // crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text('Encontre o ímovel desejado',
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.robotoMono(
+                      color: Colors.white,
+                      fontSize: 18,
+                    )),
+                SizedBox(
+                  height: 30.0,
+                ),
+                TextFormField(
+                  onChanged: (text) {},
+                  decoration: InputDecoration(
+                    fillColor: Color.fromARGB(255, 255, 255, 255),
+                    filled: true,
+                    hintText: 'Tipo',
+                    hintStyle: GoogleFonts.lato(
+                      color: Color.fromARGB(255, 97, 97, 97).withOpacity(0.5),
+                      fontSize: 20,
+                    ),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15)),
+                  ),
+                  validator: (text) {
+                    if (text!.isEmpty) {
+                      return 'Tipo inválido';
+                    }
+                  },
+                ),
+                SizedBox(
+                  height: 15.0,
+                ),
+                TextFormField(
+                  onChanged: (text) {},
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    fillColor: Color.fromARGB(255, 255, 255, 255),
+                    filled: true,
+                    hintText: 'Cidade',
+                    hintStyle: GoogleFonts.lato(
+                      color: Color.fromARGB(255, 97, 97, 97).withOpacity(0.5),
+                      fontSize: 20,
+                    ),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15)),
+                  ),
+                  validator: (text) {
+                    if (text!.isEmpty) {
+                      return 'Cidade inválida';
+                    }
+                  },
+                ),
+                SizedBox(
+                  height: 15.0,
+                ),
+                TextFormField(
+                  onChanged: (text) {},
+                  keyboardType: TextInputType.streetAddress,
+                  decoration: InputDecoration(
+                    fillColor: Color.fromARGB(255, 255, 255, 255),
+                    filled: true,
+                    hintText: 'Bairro',
+                    hintStyle: GoogleFonts.lato(
+                      color: Color.fromARGB(255, 97, 97, 97).withOpacity(0.5),
+                      fontSize: 20,
+                    ),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15)),
+                  ),
+                  validator: (text) {
+                    if (text!.isEmpty) {
+                      return 'Bairro inválido';
+                    }
+                  },
+                ),
+                SizedBox(
+                  height: 15.0,
+                ),
+                TextFormField(
+                  onChanged: (text) {},
+                  keyboardType: TextInputType.number,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    fillColor: Color.fromARGB(255, 255, 255, 255),
+                    filled: true,
+                    hintText: 'Número de Quartos',
+                    hintStyle: GoogleFonts.lato(
+                      color: Color.fromARGB(255, 97, 97, 97).withOpacity(0.5),
+                      fontSize: 20,
+                    ),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15)),
+                  ),
+                  validator: (text) {
+                    if (text!.isEmpty) {
+                      return 'Número inválido';
+                    }
+                  },
+                ),
+                SizedBox(
+                  height: 15.0,
+                ),
+                TextFormField(
+                  onChanged: (text) {},
+                  keyboardType: TextInputType.number,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    fillColor: Color.fromARGB(255, 255, 255, 255),
+                    filled: true,
+                    hintText: 'Valor',
+                    hintStyle: GoogleFonts.lato(
+                      color: Color.fromARGB(255, 97, 97, 97).withOpacity(0.5),
+                      fontSize: 20,
+                    ),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15)),
+                  ),
+                  validator: (text) {
+                    if (text!.isEmpty) {
+                      return 'Valor inválido';
+                    }
+                  },
+                ),
+                SizedBox(
+                  height: 15.0,
+                ),
+                TextFormField(
+                  onChanged: (text) {},
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    fillColor: Color.fromARGB(255, 255, 255, 255),
+                    filled: true,
+                    hintText: 'Finalidade',
+                    hintStyle: GoogleFonts.lato(
+                      color: Color.fromARGB(255, 97, 97, 97).withOpacity(0.5),
+                      fontSize: 20,
+                    ),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15)),
+                  ),
+                  validator: (text) {
+                    if (text!.isEmpty) {
+                      return 'Finalidade inválida';
+                    }
+                  },
+                ),
+                SizedBox(
+                  height: 29.0,
+                ),
+                MaterialButton(
+                  padding: EdgeInsets.all(15),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15.0))),
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                    } else {
+                      return;
+                    }
+                  },
+                  color: Color.fromARGB(255, 33, 41, 57),
+                  child: Text('Buscar',
+                      style: GoogleFonts.lato(
+                        color: Colors.white,
+                        fontSize: 20,
+                      )),
+                ),
+              ],
+            ),
+            // mainAxisAlignment: MainAxisAlignment.center,
+          ),
+        ),
+        backgroundColor: Color.fromARGB(255, 56, 65, 82),
+      ),
+    );
+  }
+}
