02c0d9050d9189696a5cc0288971fca9577306ab
WillianGiovanni <willianclemente10@gmail.com>
2022-06-21 16:40:12 -0300

    feat: create custom drawer

--- lib/screens/find_property.dart
+++ lib/screens/find_property.dart
@@ -7,6 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:imovatoapp/screens/login_page.dart';
+import 'package:imovatoapp/utils/custom_drawer.dart';
 
 class FindPropertyPage extends StatefulWidget {
   const FindPropertyPage({Key? key}) : super(key: key);
@@ -23,102 +24,7 @@ class _FindPropertyPageState extends State<FindPropertyPage> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.only(top: 70.0, left: 20.0),
-            children: [
-              Text(
-                'Olá, fulano',
-                textAlign: TextAlign.start,
-                style: GoogleFonts.robotoMono(
-                  color: Colors.white,
-                  fontSize: 20,
-                ),
-              ),
-              GestureDetector(
-                child: Text('Sair',
-                    style: GoogleFonts.robotoMono(
-                      color: Colors.blue,
-                      fontSize: 20,
-                    )),
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: ((context) => LoginPage())));
-                },
-              ),
-              SizedBox(
-                height: 100.0,
-              ),
-              GestureDetector(
-                child: Container(
-                  child: Row(
-                    children: [
-                      SvgPicture.asset('assets/homeIcon.svg'),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Text(
-                        'Início',
-                        style: GoogleFonts.robotoMono(
-                          color: Colors.white,
-                          fontSize: 20,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                onTap: () {},
-              ),
-              SizedBox(
-                height: 50.0,
-              ),
-              GestureDetector(
-                child: Container(
-                  child: Row(
-                    children: [
-                      SvgPicture.asset('assets/reservationIcon.svg'),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Text(
-                        'Reservas',
-                        style: GoogleFonts.robotoMono(
-                          color: Colors.white,
-                          fontSize: 20,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                onTap: () {},
-              ),
-              SizedBox(
-                height: 50.0,
-              ),
-              GestureDetector(
-                child: Container(
-                  child: Row(
-                    children: [
-                      SvgPicture.asset('assets/locationIcon.svg'),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Text(
-                        'Encontre um imóvel',
-                        style: GoogleFonts.robotoMono(
-                          color: Colors.white,
-                          fontSize: 20,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                onTap: () {},
-              ),
-            ],
-          ),
-          backgroundColor: Color.fromARGB(255, 56, 65, 82),
-        ),
+        drawer: CustomDrawer(),
         appBar: AppBar(
           title: Row(
             mainAxisAlignment: MainAxisAlignment.end,
